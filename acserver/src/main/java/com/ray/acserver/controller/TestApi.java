9fd9e7f0cc4447f8e6fa2ba1f88d46e096a7c1bb
zl827154659 <827154659@qq.com>
2018-01-30 15:40:02 +0800

    实现权限控制

--- acserver/src/main/java/com/ray/acserver/controller/TestApi.java
+++ acserver/src/main/java/com/ray/acserver/controller/TestApi.java
@@ -1,19 +1,25 @@
 package com.ray.acserver.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/api")
 public class TestApi {
 
+    @ResponseBody
+    @PreAuthorize("hasAuthority('ORDINARY')")
     @RequestMapping("/user")
     public String testUser() {
-        return "user";
+        return "userinfo";
     }
 
+    @ResponseBody
+    @PreAuthorize("hasAuthority('ADMIN')")
     @RequestMapping("/admin")
     public String testAdmin() {
-        return "admin";
+        return "admininfo";
     }
 }
--- acserver/src/main/java/com/ray/acserver/controller/TestController.java
+++ acserver/src/main/java/com/ray/acserver/controller/TestController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +18,7 @@ public class TestController {
     @Autowired
     UserDao userDao;
 
+    @ResponseBody
     @RequestMapping("/Ordinary")
     public String addUser() {
         User user = new User("USER2","123456");
@@ -24,12 +26,15 @@ public class TestController {
         return "success";
     }
 
+    @ResponseBody
     @RequestMapping("/Admin")
     public String addAdmin() {
         List<GrantedAuthority> authorityList = new ArrayList<GrantedAuthority>();
         Authority authority = new Authority();
         authority.setAuthority("ADMIN");
+        authorityList.add(authority);
         User user = new User("ADMIN1", "123456", authorityList);
+        userDao.addOne(user);
         return "success";
     }
 }
